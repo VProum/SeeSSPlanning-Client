5b18989a61da1d771989e6471fa3e8aa10391daf
vincentpro <vincent_pro@hotmail.fr>
2020-10-15 12:50:13 +0200

    before merge

--- src/api/apiHandler.js
+++ src/api/apiHandler.js
@@ -5,58 +5,60 @@ import axios from "axios";
 // var twitchStrategy = require("passport-twitch").Strategy;
 
 const service = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
-  withCredentials: true, // Cookie is sent to client when using this service. (used for session)
+    baseURL: process.env.REACT_APP_BACKEND_URL,
+    withCredentials: true, // Cookie is sent to client when using this service. (used for session)
+    headers: {
+        "Client-ID": process.env.TWITCH_CLIENT_ID
+    }
 });
 
 function errorHandler(error) {
-  if (error.response.data) {
-    console.log(error.response && error.response.data);
+    if (error.response.data) {
+        console.log(error.response && error.response.data);
+        throw error;
+    }
     throw error;
-  }
-  throw error;
 }
 
 export default {
-  service,
+    service,
 
-  signup(userInfo) {
-    return service
-      .post("/api/auth/signup", userInfo)
-      .then((res) => res.data)
-      .catch(errorHandler);
-  },
+    signup(userInfo) {
+        return service
+            .post("/api/auth/signup", userInfo)
+            .then((res) => res.data)
+            .catch(errorHandler);
+    },
 
-  signin(userInfo) {
-    return service
-      .post("/api/auth/signin", userInfo)
-      .then((res) => res.data)
-      .catch(errorHandler);
-  },
+    signin(userInfo) {
+        return service
+            .post("/api/auth/signin", userInfo)
+            .then((res) => res.data)
+            .catch(errorHandler);
+    },
 
-  isLoggedIn() {
-    return service
-      .get("/api/auth/isLoggedIn")
-      .then((res) => res.data)
-      .catch(errorHandler);
-  },
+    isLoggedIn() {
+        return service
+            .get("/api/auth/isLoggedIn")
+            .then((res) => res.data)
+            .catch(errorHandler);
+    },
 
-  logout() {
-    return service
-      .get("/api/auth/logout")
-      .then((res) => res.data)
-      .catch(errorHandler);
-  },
+    logout() {
+        return service
+            .get("/api/auth/logout")
+            .then((res) => res.data)
+            .catch(errorHandler);
+    },
 
-  getItems() {
-    return service
-      .get("/api/items")
-      .then((res) => res.data)
-      .catch(errorHandler);
-  },
+    getItems() {
+        return service
+            .get("/api/items")
+            .then((res) => res.data)
+            .catch(errorHandler);
+    },
 
-testPouet(){
-  return service.get("https://id.twitch.tv/oauth2/authorize?client_id=m3vo1t7dvgtkfb9korsfpzlgjrh5vk&redirect_uri=http://localhost:8080&response_type=token&scope=user:read:email").then(apires => console.log(apires));
-}
-
+    testPouet() {
+        return service.get("https://id.twitch.tv/oauth2/authorize?client_id=m3vo1t7dvgtkfb9korsfpzlgjrh5vk&redirect_uri=http://localhost:8080&response_type=token&scope=user:read:email").then(apires => console.log(apires));
+    }
 };
