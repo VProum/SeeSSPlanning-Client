01d9af33317798ba491a338c5e15c1fac49925ff
Marcha <marchaf@gmail.com>
2020-10-16 09:46:37 +0200

    add editForm

---
+++ src/components/Forms/FormEditSchedule.jsx
@@ -0,0 +1,240 @@
+import React, { Component } from "react";
+import { withUser } from "../Auth/withUser";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import DateFnsUtils from "@date-io/date-fns";
+import Typography from "@material-ui/core/Typography";
+import Slider from "@material-ui/core/Slider";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import SaveIcon from "@material-ui/icons/Save";
+import Button from "@material-ui/core/Button";
+
+const useStyles = (theme) => ({
+  root: {
+    flexGrow: 1,
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "75%",
+    },
+    "& .MuiFormControl-root": {
+      width: "75%",
+    },
+  },
+
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+});
+
+class EditSchedule extends React.Component {
+  state = {
+    hourDay: null,
+    duration: 3,
+    weekday: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      lastname: e.target.value,
+    });
+  };
+
+  handleDateChange = (date) => {
+    this.setState({
+      hourDay: date,
+    });
+  };
+
+  valuetext = (event, value) => {
+     let tmp = value;
+     this.setState({
+         duration: tmp,
+     })
+  };
+
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    this.props.addSchedule(this.state);
+
+  };
+
+  handleSelectChange = (e) => {
+    
+    this.setState({
+        weekday: e.target.value
+    })
+  };
+
+
+  render() {
+    const { classes } = this.props;
+
+    const marks = [
+      {
+        value: 1,
+        label: "1h",
+      },
+      {
+        value: 2,
+        label: "2h",
+      },
+      {
+        value: 3,
+        label: "3h",
+      },
+      {
+        value: 4,
+        label: "4h",
+      },
+      {
+        value: 5,
+        label: "5h",
+      },
+      {
+        value: 6,
+        label: "6h",
+      },
+      {
+        value: 7,
+        label: "7h",
+      },
+      {
+        value: 8,
+        label: "8h",
+      },
+      {
+        value: 9,
+        label: "9h",
+      },
+      {
+        value: 10,
+        label: "10h",
+      },
+      {
+        value: 11,
+        label: "11h",
+      },
+      {
+        value: 12,
+        label: "12h",
+      },
+    ];
+
+    return (
+      <div className={`${classes.root} margin-left`}>
+        <Grid container spacing={3} style={{ marginTop: "1%" }}>
+          <Grid item xs={6}>
+            Add a stream in schedule
+            <br />
+            <form className={classes.root} onSubmit={this.handleSubmit}>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <Grid container justify="flex-start">
+                  <KeyboardTimePicker
+                    id="hourDay"
+                    name="hourDay"
+                    label="Start Hour"
+                    value={this.state.hourDay}
+                    onChange={this.handleDateChange}
+                    KeyboardButtonProps={{ "aria-label": "change time" }}
+                  />
+                </Grid>
+              </MuiPickersUtilsProvider>
+{/* 
+              <FormPicker name="hourDay" 
+              defaultvalue={this.state.hourDay}
+               value={Date}
+               onChange={this.handleDateChange}
+               >Start Hour</FormPicker>  */}
+
+              <br />
+
+              <Typography id="discrete-slider-always" gutterBottom>
+               Duration
+              </Typography>
+              <Slider
+                 id="duration"
+                name="duration"
+                
+              
+                aria-labelledby="discrete-slider-always"
+                step={1}
+                min={1}
+                max={12}
+                marks={marks}
+                valueLabelDisplay="on" 
+                value={this.state.duration}
+                onChange={this.valuetext}
+              />
+
+              <br />
+              {/* <TextField
+                id="outlined-basic"
+                defaultValue="pouet"
+                name="tata"
+                label="tata"
+                variant="outlined"
+                onChange={this.handlechange}
+              /> */}
+
+            <FormControl className={classes.formControl}>
+      
+        <InputLabel id="weekday">Weekdays</InputLabel>
+        <Select
+          labelId="weekday"
+          id="weekday"
+          name="weekday"
+          value={this.state.weekday}
+          onChange={this.handleSelectChange}
+        >
+          <MenuItem value="Monday">Monday</MenuItem>
+          <MenuItem value="Tuesday">Tuesday</MenuItem>
+          <MenuItem value="Wednesday">Wednesday</MenuItem>
+          <MenuItem value="Thursday">Thursday</MenuItem>
+          <MenuItem value="Friday">Friday</MenuItem>
+          <MenuItem value="Saturday">Saturday</MenuItem>
+          <MenuItem value="Sunday">Sunday</MenuItem>
+        </Select>
+      </FormControl> 
+
+              <br />
+              {/* <FormInput name="lastName" value={this.state.lastName} onChange={this.handleTest}>lastName</FormInput> */}
+              
+
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                className={classes.button}
+                startIcon={<SaveIcon />}
+                style={{ marginTop: "5%" }}
+                type="submit"
+              >
+                Save
+              </Button>
+
+              {/* <Field name="lastName" value={this.state.lastName}>LastName</Field> */}
+            </form>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>xs=6</Paper>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default withStyles(useStyles)(withUser(EditSchedule));
--- src/pages/EditSchedule.jsx
+++ src/pages/EditSchedule.jsx
@@ -1,166 +1,30 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
+import FormEditSchedule from '../components/Forms/FormEditSchedule'
 import { withUser } from "../components/Auth/withUser";
-import { withStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import TextField from "@material-ui/core/TextField";
-import DateFnsUtils from "@date-io/date-fns";
-import Typography from "@material-ui/core/Typography";
-import Slider from "@material-ui/core/Slider";
 
-import {
-  MuiPickersUtilsProvider,
-  KeyboardTimePicker,
-  KeyboardDatePicker,
-} from "@material-ui/pickers";
 
-const useStyles = (theme) => ({
-  root: {
-    flexGrow: 1,
-    "& .MuiTextField-root": {
-      margin: theme.spacing(1),
-      width: "75%",
-    },
-    "& .MuiFormControl-root": {
-      width: "75%",
-    },
-  },
+class EditSchedule extends Component {
+    state = {
+     hourDay: null,
+     duration: 3,
+     weekday: "",
+    }
 
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
-});
+    handleAdd = (schedule) => {
+        console.log("Schedule ", schedule);
+        // this.setState({
 
-class EditSchedule extends React.Component {
-  state = {
-    tata: "",
-  };
+        // });
+    };
 
-  handleChange = (e) => {
-    console.log("name: ", e.target.name, "    value: ", e.target.value);
-  };
-
-  handleDateChange = (date) => {
-    console.log(date);
-  };
-
-  valuetext = (value) => {
-      let oldValue ="";
-      if(oldValue !== value){
-          console.log(value);
-        return `${value}°C`;
-      }
-  };
-
-  render() {
-    const { classes } = this.props;
-
-    const marks = [
-      {
-        value: 1,
-        label: "1h",
-      },
-      {
-        value: 2,
-        label: "2h",
-      },
-      {
-        value: 3,
-        label: "3h",
-      },
-      {
-        value: 4,
-        label: "4h",
-      },
-      {
-        value: 5,
-        label: "5h",
-      },
-      {
-        value: 6,
-        label: "6h",
-      },
-      {
-        value: 7,
-        label: "7h",
-      },
-      {
-        value: 8,
-        label: "8h",
-      },
-      {
-        value: 9,
-        label: "9h",
-      },
-      {
-        value: 10,
-        label: "10h",
-      },
-      {
-        value: 11,
-        label: "11h",
-      },
-      {
-        value: 12,
-        label: "12h",
-      },
-    ];
-
-    return (
-      <div className={`${classes.root} margin-left`}>
-        <Grid container spacing={3} style={{ marginTop: "1%" }}>
-          <Grid item xs={6}>
-            Add a stream in schedule
-            <br />
-            <form className={classes.root} noValidate autoComplete="off">
-              <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <Grid container justify="flex-start">
-                  <KeyboardTimePicker
-                    id="time-picker"
-                    label="Start Hour"
-                    //  value={selectedDate}
-                    onChange={this.handleDateChange}
-                    KeyboardButtonProps={{ "aria-label": "change time" }}
-                  />
-                </Grid>
-              </MuiPickersUtilsProvider>
-
-              <br />
-
-              <Typography id="discrete-slider-always" gutterBottom>
-                Always visible
-              </Typography>
-              <Slider
-                defaultValue={3}
-                getAriaValueText={this.valuetext}
-                aria-labelledby="discrete-slider-always"
-                step={1}
-                min={1}
-                max={12}
-                marks={marks}
-                valueLabelDisplay="on"  
-              />
-
-              <br />
-              <TextField
-                id="outlined-basic"
-                defaultValue="pouet"
-                name="tata"
-                label="tata"
-                variant="outlined"
-                onChange={this.handlechange}
-              />
-            </form>
-          </Grid>
-          <Grid item xs={6}>
-            <Paper className={classes.paper}>xs=6</Paper>
-          </Grid>
-        </Grid>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>POUE POTER</h1>
+                <FormEditSchedule addSchedule={this.handleAdd}/>
+            </div>
+        )
+    }
 }
 
-export default withStyles(useStyles)(withUser(EditSchedule));
+export default withUser(EditSchedule);
